976e92a2d64d189e2b590e40577c56b94d3b51b5
Clement Denis <cdenis@student.42.fr>
2016-05-10 20:54:40 +0200

    fix the cook, prepare loading repo from url hash

---
+++ src/lib/hash.js
@@ -0,0 +1,31 @@
+const observ = require('lib/emiter/observ')
+const window = require('global/window')
+
+const hStart = /^#\//
+const enforceTrailingSlash = h => h[h.length - 1] !== '/' ? h +'/' : h
+const hash = observ.format(observ.check(), h => {
+  if (!h) return '#/'
+  if (h[0] !== '#') {
+    if (h[0] === '/') return enforceTrailingSlash('#'+ h)
+    return enforceTrailingSlash('#/'+ h)
+  }
+  if (h[1] !== '/') return enforceTrailingSlash('#/'+ h.slice(1))
+  return enforceTrailingSlash(h)
+})
+
+let inPopState = false
+window.addEventListener('popstate', () => {
+  inPopState = true
+  hash.set(window.location.hash)
+})
+
+hash(h => {
+  if (inPopState) return inPopState = false
+  window.history.pushState(null, document.title, '/'+ h)
+})
+
+hash.set(window.location.hash)
+
+hash.parts = () => hash().slice(2).split('/').filter(Boolean)
+
+module.exports = hash
