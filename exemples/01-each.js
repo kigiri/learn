62e9b9c8d4d90a8f39ce88e131c2b661ea5fec9e
Clément <cdenis@student.42.fr>
2016-05-23 16:15:14 +0200

    Saving exemple of exemples 01-each.js - set exemple

--- exemples/01-each.js
+++ exemples/01-each.js
@@ -1,3 +1,9 @@
-const each = (fn, arr) => {
+// CODE HERE
+// to help you, here the function signature I want :
 
+// fn is the give function
+// collection is the array for the first test
+const each = (fn, collection) => {
+  // ???
+  //
 }
