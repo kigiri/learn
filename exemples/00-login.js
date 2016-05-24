5e85e961e27154b6962bd230c9ca67b2ce0e4e51
Clément <cdenis@student.42.fr>
2016-05-25 01:25:11 +0200

    Saving exemple of exemples 00-login.js - Move text to the exemple

--- exemples/00-login.js
+++ exemples/00-login.js
@@ -1,7 +1,9 @@
 /*
  * Instructions :
- * - Open your console and call the function git.login like so :
+ * - Open your browser console and call the function git.login like so :
  *     git.login('username', 'password')
+ *       -- Signup here https://github.com/join
+ *
  *     git.login('ebfc2f58210d28a8dc35b38a54222c2c77c1da40')
  *       -- OAuth Token
  *       -- Make yours here https://github.com/settings/tokens/new
@@ -10,10 +12,14 @@
  *     git.login('username')
  *       -- will prompt for password so it's not in the console history
  *
- * - To load next exercise, call the function next.
- * - You can call next and skip any exercise use prev to go back
- * - You don't need to be login, it's only for saving progress.
- *   Also note that github limit unauthenticated api calls to 60 per hour
+ * -- To load next exercise, call the function next.
+ * -- You can call next and skip any exercise use prev to go back
+ * -- You don't need to be login, it's only for saving progress.
+ *    Also note that github limit unauthenticated api calls to 60 per hour
+ *
+ *
+ * All API calls are in HTTPS so your password is not vulnerable.
+ * If you still feel uneasy, you can make a trash account just for this.
  *
  * Once you are login, your credentials are stored in the localStorage.
  *
@@ -21,5 +27,7 @@
  * -- git.logout() (call git.logout.toString() to see what does the function)
  * -- localStorage.clear() (will remove your local progress, git.commit() first)
  *
+ * You can check all the api calls I do here :
+ * -> https://github.com/kigiri/learn/blob/master/src/helper/github.js#L35
  */
  
