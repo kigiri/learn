58a1a254968db0d5f67257f5a87a5aeb23ecd89f
Clément <cdenis@student.42.fr>
2016-05-25 01:25:25 +0200

    Saving test of tests 00-login.js - Simplify test code

--- tests/00-login.js
+++ tests/00-login.js
@@ -2,19 +2,8 @@
  * Category 00 : Basics
  * Exercise 00 : Login
  *
- * You can only interact from the editor and the console.
- * You want to have the console open at all time, so do that first.
- *
  * This website is built upon the Github API.
- * If you are not login, your API calls are limited to 60 per hours
- * and you can't store your progress.
  *
- * You can signin here for free if you don't already have an account :
- * -> https://github.com/join
+ * This test is useless, skip it !
  *
- * All API calls are in HTTPS so your password is not vulnerable.
- * If you still feel uneasy, you can make a trash account just for this.
- *
- * You can check all the api calls I do here :
- * -> https://github.com/kigiri/learn/blob/master/src/helper/github.js#L35
  */
